436e9b73191c36c75e346b15fc1d93286b9e5dc3
Nbiba Bedis <bedisnbiba@gmail.com>
2022-09-29 20:58:38 +0100

    fmt

--- ytfview.ts
+++ ytfview.ts
@@ -1,4 +1,4 @@
-import { SizeHint, Webview } from "https://deno.land/x/webview@0.7.5/mod.ts";
+import { Webview } from "https://deno.land/x/webview@0.7.5/mod.ts";
 import { dirname, join } from "https://deno.land/std@0.158.0/path/mod.ts";
 
 const worker = new Worker(
