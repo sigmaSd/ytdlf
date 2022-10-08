ff4743a4dad218bb63cb1132b0615942ec6805ad
Nbiba Bedis <bedisnbiba@gmail.com>
2022-10-08 13:58:21 +0100

    clean

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -1,10 +1,3 @@
-self.addEventListener("fetch", (e) => {
-  e.respondWith((async () => {
-    try {
-      return await fetch(e.request);
-    } catch {
-      // this also fools chrome offline support detection
-      return new Response("Youtube Downloder requires network");
-    }
-  })());
+self.addEventListener("fetch", () => {
+  // needed to make the app installable
 });
