18660c590050d7d61a8004bac073337c6358ac0c
Nbiba Bedis <bedisnbiba@gmail.com>
2022-10-08 17:25:29 +0100

    share target support works

--- routes/share-target.tsx
+++ routes/share-target.tsx
@@ -4,9 +4,6 @@ import App from "../islands/App.tsx";
 export const handler: Handlers = {
   async GET(req, ctx) {
     const url = new URL(req.url).searchParams.get("text");
-    console.log("called from share");
-    console.log(url);
-
     const resp = await ctx.render({ url });
     return resp;
   },
