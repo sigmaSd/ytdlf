1417c4ad95c8a6ee2349dbb59c25a58c68a7904d
Nbiba Bedis <bedisnbiba@gmail.com>
2022-10-08 14:05:08 +0100

    mobile freind

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <div>
       <Head>
         <link rel="manifest" href="/pwa/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       </Head>
       <script defer src="/pwa/app.js" />
       <App />;
